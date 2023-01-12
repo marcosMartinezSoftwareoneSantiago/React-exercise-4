83b233e7ca88ccac58a7bd072a945f6f33d6330f
marcosMartinezSoftwareoneSantiago <Marcos.Martinez@Softwareone.com>
2023-01-12 12:53:52 +0100

    It works but some bootstrap must be added

--- src/components/FunctionComponent.jsx
+++ src/components/FunctionComponent.jsx
@@ -1,3 +1,12 @@
 import React from "react";
 
-export const FunctionComponent = 3;
+const retorna = (param) => param;
+
+export const FunctionComponent = () =>{
+
+    return(
+        <header  className="justify-content-center navbar fixed-top bg-dark">
+            <h1 className="text-light">{retorna("Text of the header had being interpoled")}</h1>
+        </header>
+    );
+};
---
+++ src/components/VariableComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+let title = "El título de la página";
+export const VariableComponent = () => {
+
+    return(
+        <h3 className="row">{title}</h3>
+    );
+};
